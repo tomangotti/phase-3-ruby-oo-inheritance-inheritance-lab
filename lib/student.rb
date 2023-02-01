c1b71ebabd88b3fc03dd2fbc2904b6267711a695
thomas <tom.angotti11@gmail.com>
2023-02-01 11:51:05 -0700

    commit tree 98ee7d01f6ec790c86655b2bb3167c63f3fad37e

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,23 @@
-class Student 
+require_relative "user.rb"
+
+class Student < User
+
+    attr_accessor :knowledge
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(subject)
+        knowledge << subject
+    end
+
+    def knowledge
+        @knowledge
+    end
 
 end
+
+tom = Student.new
+tom.learn("math")
+puts tom.knowledge
